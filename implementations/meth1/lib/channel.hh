215b72b02685731fb2bccb99c9a76dbef1787853
David Terei <code@davidterei.com>
2015-07-28 13:00:26 -0700

    Add suport for synchronous channels

--- implementations/meth1/lib/channel.hh
+++ implementations/meth1/lib/channel.hh
@@ -1,6 +1,7 @@
 #ifndef CHANNEL_HH
 #define CHANNEL_HH
 
+#include <algorithm>
 #include <chrono>
 #include <exception>
 #include <iostream>
@@ -13,8 +14,7 @@
 /* We hide the inner implementation of channel that shouldn't be moved */
 namespace internal {
 
-  /* A channel provides a buffered communication pipe. We only supported buffered
-   * channels for now, not synchronous ones.
+  /* A channel provides a buffered communication pipe.
    */ 
   template <typename T>
   class Channel_
@@ -30,25 +30,31 @@ namespace internal {
 
   private:
     std::mutex mtx_;
-    std::condition_variable cv_;
+    std::condition_variable send_cv_;
+    std::condition_variable recv_cv_;
     std::unique_ptr<T[]> slots_;
     size_t size_;
     size_t used_;
     size_t wptr_;
     size_t rptr_;
     bool closed_;
+    size_t send_wait_; // only for synchronous channels
+    size_t recv_wait_; // only for synchronous channels
 
   public:
 
     Channel_( size_t buf = 1 )
       : mtx_{}
-      , cv_{}
-      , slots_{new T[buf]}
+      , send_cv_{}
+      , recv_cv_{}
+      , slots_{new T[buf > 1 ? buf : 1]}
       , size_{buf}
       , used_{0}
       , wptr_{0}
       , rptr_{0}
       , closed_{false}
+      , send_wait_{0}
+      , recv_wait_{0}
     {}
 
     /* No move or copy */
@@ -61,7 +67,8 @@ namespace internal {
     {
       std::unique_lock<std::mutex> lck( mtx_ );
       closed_ = true;
-      cv_.notify_all();
+      send_cv_.notify_all();
+      recv_cv_.notify_all();
     }
 
     template <typename U>
@@ -72,17 +79,37 @@ namespace internal {
         throw closed_error();
       }
 
-      if ( used_ == size_ ) {
-        cv_.wait( lck );
-        if ( closed_ ) {
-          throw closed_error();
+      if ( size_ == 0 ) {
+        // synchronous
+        while ( true ) {
+          if ( recv_wait_ == 0 || used_ == 1 ) {
+            send_wait_++;
+            send_cv_.wait( lck );
+            if ( closed_ ) {
+              throw closed_error();
+            }
+            send_wait_--;
+          } else {
+            slots_[0] = std::forward<U>( u );
+            used_ = 1;
+            recv_cv_.notify_one();
+            return;
+          }
         }
-      }
-      cv_.notify_one();
+      } else {
+        // asynchronous
+        if ( used_ == size_ ) {
+          send_cv_.wait( lck );
+          if ( closed_ ) {
+            throw closed_error();
+          }
+        }
+        recv_cv_.notify_one();
 
-      used_++;
-      slots_[wptr_] = std::forward<U>( u );
-      wptr_ = (wptr_ + 1) % size_;
+        used_++;
+        slots_[wptr_] = std::forward<U>( u );
+        wptr_ = (wptr_ + 1) % size_;
+      }
     }
 
     inline T recv( void )
@@ -92,18 +119,39 @@ namespace internal {
         throw closed_error();
       }
 
-      if ( used_ == 0 ) {
-        cv_.wait( lck );
-        if ( closed_ ) {
-          throw closed_error();
+      if ( size_ == 0 ) {
+        // synchronous
+        while ( true ) {
+          if ( used_ == 0 ) {
+            recv_wait_++;
+            if ( send_wait_ > 0 ) {
+              send_cv_.notify_one();
+            }
+            recv_cv_.wait( lck );
+            if ( closed_ ) {
+              throw closed_error();
+            }
+            recv_wait_--;
+          } else {
+            used_ = 0;
+            return slots_[0];
+          }
         }
+      } else {
+        // asynchronous
+        if ( used_ == 0 ) {
+          recv_cv_.wait( lck );
+          if ( closed_ ) {
+            throw closed_error();
+          }
+        }
+        send_cv_.notify_one();
+        
+        used_--;
+        T t = slots_[rptr_];
+        rptr_ = (rptr_ + 1) % size_;
+        return t;
       }
-      cv_.notify_one();
-      
-      used_--;
-      T t = slots_[rptr_];
-      rptr_ = (rptr_ + 1) % size_;
-      return t;
     }
   };
 }
