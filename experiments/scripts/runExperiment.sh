f6810a6dd1b43bf02a817d4c3f2a41e55a2e19e8
David Terei <code@davidterei.com>
2015-08-12 01:36:17 -0700

    Fix ssh bug in runExperiment script.

--- experiments/scripts/runExperiment.sh
+++ experiments/scripts/runExperiment.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 SSH="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+SCP="scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 KEY=$( hostname )
 FILE=$1
 SAVE=$2
 N=$3
+MACHINE=$4
 
-if [ -z "${FILE}" -o -z "${SAVE}" -o -z "${N}" ]; then
-  echo "runExperiment.sh <cluster file> <log path> <nodes>"
+if [ -z "${FILE}" -o -z "${SAVE}" -o -z "${N}" -o -z "${MACHINE}" ]; then
+  echo "runExperiment.sh <cluster file> <log path> <nodes> <machine>"
   exit 1
 fi
 
 # launch
-./launchBAD.rb -f ${FILE} -k ${KEY} -c ${N} 'Measure-%d' -d measure.tar.gz
+./launchBAD.rb -f ${FILE} -k ${KEY} -c ${N} 'Measure-%d' -d measure.tar.gz -i ${MACHINE}
 
 source ${FILE}
 
@@ -26,7 +28,7 @@ wait
 for i in `seq 1 $MN`; do
   declare MV="M${i}"
   mkdir -p ${SAVE}/${i}/
-  scp ubuntu@${!MV}:log_* ${SAVE}/${i}/ &
+  ${SCP} ubuntu@${!MV}:log_* ${SAVE}/${i}/ &
 done
 wait
 
