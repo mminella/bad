ffc2293fe603a0789687d7d87003c81f9ce8e88a
David Terei <code@davidterei.com>
2015-08-17 18:47:11 -0700

    Rename meth2 experiment files to better group related ones.

--- implementations/meth2/experiments/index4.cc
+++ implementations/meth2/experiments/index4.cc
@@ -1,122 +1,50 @@
 /**
  * Basic index building test.
  *
- * - Uses Overlapping IO.
+ * - Uses Overlapped file IO.
  * - Uses libsort record type.
- * - Uses threadpool for parallelism.
- * - Use own chunking strategy.
+ * - Uses google::btree.
  */
-#include <algorithm>
-#include <future>
-#include <chrono>
-#include <vector>
-
-#include "buffered_io.hh"
 #include "exception.hh"
 #include "file.hh"
+#include "linux_compat.hh"
 #include "overlapped_rec_io.hh"
 #include "timestamp.hh"
-#include "threadpool.hh"
 
 #include "record.hh"
+#include "btree_set.h"
 
 using namespace std;
 
-using rec_i = vector<Record>::iterator;
-using rec_ip = pair<rec_i, rec_i>;
-using f_rec_ip = future<rec_ip>;
-using s_rec_ip = shared_ptr<f_rec_ip>;
-
-rec_ip mysort( uint64_t i, rec_i begin, rec_i end )
-{
-  (void) i;
-  // cout << "start sort, " << i << ", " << timestamp<ms>() << endl;
-  rec_sort( begin, end );
-  // cout << "end sort, " << i << ", " << timestamp<ms>() << endl;
-  rec_sort( begin, end );
-  return make_pair( begin, end );
-}
-
-rec_ip mymerge( uint64_t i, s_rec_ip left, s_rec_ip right )
-{
-  (void) i;
-  // cout << "queue merge, " << i << ", " << timestamp<ms>() << endl;
-  auto l = left->get();
-  auto r = right->get();
-  // cout << "start merge, " << i << ", " << timestamp<ms>() << endl;
-  inplace_merge( l.first, l.second, r.second );
-  // cout << "end merge, " << i << ", " << timestamp<ms>() << endl;
-  return make_pair( l.first, r.second );
-}
-
 void run( char * fin )
 {
-  // get in/out files
-  File fdi( fin, O_RDONLY );
-  OverlappedRecordIO<Rec::SIZE> rio( fdi );
-  ThreadPool tp;
+  // open file
+  File file( fin, O_RDONLY | O_DIRECT );
+  OverlappedRecordIO<Rec::SIZE> rio( file );
 
-  // start reading
+  // stats
+  size_t nrecs = file.size() / Rec::SIZE;
+  cout << "size, " << nrecs << endl;
+  cout << "sizeof, " << sizeof(RecordLoc) << endl;
+  cout << endl;
+
   rio.rewind();
 
-  size_t nrecs = fdi.size() / Rec::SIZE;
-  size_t split = 8;
-  size_t chunk = nrecs / split;
-  vector<Record> recs;
-  recs.reserve( nrecs );
-  vector<f_rec_ip> merges( split / 2 );
-
+  // index
+  btree::btree_set<RecordLoc, less<RecordLoc>, allocator<RecordLoc>, 227> bt;
   auto t1 = time_now();
-  uint64_t split_i = 0;
-  bool run = true;
-  for ( uint64_t i = 0; run; i++ ) {
-    const char * r = rio.next_record();
-    if ( fdi.eof() || r == nullptr ) {
-      run = false;
-    } else {
-      recs.emplace_back( r, i * Rec::SIZE + Rec::KEY_LEN );
+  for ( uint64_t i = 0;; i++ ) {
+    const uint8_t * r = (const uint8_t *) rio.next_record();
+    if ( r == nullptr ) {
+      break;
     }
-
-    if ( i != 0 and ( i % chunk == 0 or !run )
-        and recs.begin() + split_i < recs.end() ) {
-
-      // start the sort
-      auto fut = tp.enqueue( &mysort, split_i / chunk,
-        recs.begin() + split_i,
-        min( recs.begin() + split_i + chunk, recs.end() ) );
-      split_i += chunk;
-
-      // queue up the needed merges
-      for ( auto & m : merges ) {
-        if ( m.valid() ) {
-          fut = tp.enqueue( &mymerge, split_i / chunk,
-            make_shared<f_rec_ip>( move( m ) ),
-            make_shared<f_rec_ip>( move( fut ) ) );
-        } else {
-          m = move( fut );
-          break;
-        }
-      }
-    }
+    bt.insert( {r, i} );
   }
   auto t2 = time_now();
 
-  // final merge
-  auto tb = time_now();
-  for ( const auto & m : merges ) {
-    if ( m.valid() ) { m.wait(); }
-  }
-  auto t3 = time_now();
-
-  // stats
+  // timings
   auto t21 = time_diff<ms>( t2, t1 );
-  auto t32 = time_diff<ms>( t3, t2 );
-  auto t31 = time_diff<ms>( t3, t1 );
-
-  cout << "Read  took " << t21 << "ms" << endl;
-  cout << "Sort  took " << t32 << "ms" << endl;
-  cout << "Merge took " << time_diff<ms>( t3, tb ) << "ms" << endl;
-  cout << "Total took " << t31 << "ms" << endl;
+  cout << "Total took " << t21 << "ms" << endl;
 }
 
 void check_usage( const int argc, const char * const argv[] )
--- implementations/meth2/experiments/index6.cc
+++ implementations/meth2/experiments/index6.cc
@@ -1,50 +1,126 @@
 /**
  * Basic index building test.
  *
- * - Uses Overlapped file IO.
+ * - Uses Overlapping IO.
  * - Uses libsort record type.
- * - Uses google::btree.
+ * - Uses threadpool for parallelism.
+ * - Use own chunking strategy.
+ * 
+ * This doesn't work so well, likely as inplace_merge is quite slow. Could try
+ * again with more memory and external merge strategy. May also want to merge
+ * more than one sorted buffer at a time and control that trade-off better.
  */
+#include <algorithm>
+#include <future>
+#include <chrono>
+#include <vector>
+
+#include "buffered_io.hh"
 #include "exception.hh"
 #include "file.hh"
-#include "linux_compat.hh"
 #include "overlapped_rec_io.hh"
 #include "timestamp.hh"
+#include "threadpool.hh"
 
 #include "record.hh"
-#include "btree_set.h"
 
 using namespace std;
 
+using rec_i = vector<Record>::iterator;
+using rec_ip = pair<rec_i, rec_i>;
+using f_rec_ip = future<rec_ip>;
+using s_rec_ip = shared_ptr<f_rec_ip>;
+
+rec_ip mysort( uint64_t i, rec_i begin, rec_i end )
+{
+  (void) i;
+  // cout << "start sort, " << i << ", " << timestamp<ms>() << endl;
+  rec_sort( begin, end );
+  // cout << "end sort, " << i << ", " << timestamp<ms>() << endl;
+  rec_sort( begin, end );
+  return make_pair( begin, end );
+}
+
+rec_ip mymerge( uint64_t i, s_rec_ip left, s_rec_ip right )
+{
+  (void) i;
+  // cout << "queue merge, " << i << ", " << timestamp<ms>() << endl;
+  auto l = left->get();
+  auto r = right->get();
+  // cout << "start merge, " << i << ", " << timestamp<ms>() << endl;
+  inplace_merge( l.first, l.second, r.second );
+  // cout << "end merge, " << i << ", " << timestamp<ms>() << endl;
+  return make_pair( l.first, r.second );
+}
+
 void run( char * fin )
 {
-  // open file
-  File file( fin, O_RDONLY | O_DIRECT );
-  OverlappedRecordIO<Rec::SIZE> rio( file );
+  // get in/out files
+  File fdi( fin, O_RDONLY );
+  OverlappedRecordIO<Rec::SIZE> rio( fdi );
+  ThreadPool tp;
 
-  // stats
-  size_t nrecs = file.size() / Rec::SIZE;
-  cout << "size, " << nrecs << endl;
-  cout << "sizeof, " << sizeof(RecordLoc) << endl;
-  cout << endl;
-
+  // start reading
   rio.rewind();
 
-  // index
-  btree::btree_set<RecordLoc, less<RecordLoc>, allocator<RecordLoc>, 227> bt;
+  size_t nrecs = fdi.size() / Rec::SIZE;
+  size_t split = 8;
+  size_t chunk = nrecs / split;
+  vector<Record> recs;
+  recs.reserve( nrecs );
+  vector<f_rec_ip> merges( split / 2 );
+
   auto t1 = time_now();
-  for ( uint64_t i = 0;; i++ ) {
-    const uint8_t * r = (const uint8_t *) rio.next_record();
-    if ( r == nullptr ) {
-      break;
+  uint64_t split_i = 0;
+  bool run = true;
+  for ( uint64_t i = 0; run; i++ ) {
+    const char * r = rio.next_record();
+    if ( fdi.eof() || r == nullptr ) {
+      run = false;
+    } else {
+      recs.emplace_back( r, i * Rec::SIZE + Rec::KEY_LEN );
     }
-    bt.insert( {r, i} );
+
+    if ( i != 0 and ( i % chunk == 0 or !run )
+        and recs.begin() + split_i < recs.end() ) {
+
+      // start the sort
+      auto fut = tp.enqueue( &mysort, split_i / chunk,
+        recs.begin() + split_i,
+        min( recs.begin() + split_i + chunk, recs.end() ) );
+      split_i += chunk;
+
+      // queue up the needed merges
+      for ( auto & m : merges ) {
+        if ( m.valid() ) {
+          fut = tp.enqueue( &mymerge, split_i / chunk,
+            make_shared<f_rec_ip>( move( m ) ),
+            make_shared<f_rec_ip>( move( fut ) ) );
+        } else {
+          m = move( fut );
+          break;
+        }
+      }
+    }
   }
   auto t2 = time_now();
 
-  // timings
+  // final merge
+  auto tb = time_now();
+  for ( const auto & m : merges ) {
+    if ( m.valid() ) { m.wait(); }
+  }
+  auto t3 = time_now();
+
+  // stats
   auto t21 = time_diff<ms>( t2, t1 );
-  cout << "Total took " << t21 << "ms" << endl;
+  auto t32 = time_diff<ms>( t3, t2 );
+  auto t31 = time_diff<ms>( t3, t1 );
+
+  cout << "Read  took " << t21 << "ms" << endl;
+  cout << "Sort  took " << t32 << "ms" << endl;
+  cout << "Merge took " << time_diff<ms>( t3, tb ) << "ms" << endl;
+  cout << "Total took " << t31 << "ms" << endl;
 }
 
 void check_usage( const int argc, const char * const argv[] )
