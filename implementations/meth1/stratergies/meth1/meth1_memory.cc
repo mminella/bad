4cacb734bd506c64d0f2b86e0fe93a99f9c95252
David Terei <code@davidterei.com>
2015-08-29 14:13:36 -0700

    Account for external fragmentation in dynamic memory calcs.

--- implementations/meth1/stratergies/meth1/meth1_memory.cc
+++ implementations/meth1/stratergies/meth1/meth1_memory.cc
@@ -7,6 +7,8 @@
 #include "overlapped_io.hh"
 #include "util.hh"
 
+#include "record.hh"
+
 #include "meth1_memory.hh"
 #include "node.hh"
 
@@ -25,22 +27,49 @@ size_t num_of_disks( void )
   return max( (size_t) atoll( buf ), (size_t) 1 );
 }
 
+uint64_t calc_value_size( void )
+{
+  // Try to figure out how tightly we can allocate values (i.e., external
+  // fragementation)
+  vector <uint8_t *> vals;
+  vector <uint64_t> diffs;
+  for ( size_t i = 0; i < 6; i++ ) {
+    vals.push_back( Rec::alloc_val() );
+  }
+
+  for ( auto v : vals ) {
+    Rec::dealloc_val( v );
+  }
+
+  for ( size_t i = 1; i < vals.size(); i++ ) {
+    diffs.push_back( uint64_t( vals[i] - vals[i-1] ) );
+  }
+
+  uint64_t val_len = *min_element( diffs.begin(), diffs.end() );
+  cout << "record-value, " << val_len << endl;
+  return min( val_len, 2 * Rec::VAL_LEN );
+}
+
 /* figure out max chunk size available */
 uint64_t calc_record_space( void )
 {
   // XXX: we should really do this at the backend node, not the client, but the
   // RPC interface isn't really setup that way.
   uint64_t memFree = memory_free();
+  uint64_t val_len = calc_value_size();
 
   // subtract reserved mem for OS & misc
   memFree -= Knobs::MEM_RESERVE;
   // subtract disk read buffers
-  memFree -= OverlappedIO::BUFFER_SIZE * num_of_disks();
-  // divide by record size
-  memFree /= ( uint64_t( 2 ) * uint64_t( sizeof(Node::RR) ) + Rec::VAL_LEN );
-  // subtract the extra sort buffer
-  memFree = ( memFree * Knobs::SORT_MERGE_RATIO )
-    / ( Knobs::SORT_MERGE_RATIO + 1 );
+  memFree -= ( OverlappedIO::BUFFER_SIZE * num_of_disks() );
+
+  // divisor for r2 & r3 merge buffers
+  uint64_t div1 = uint64_t( 2 ) * uint64_t( sizeof( Node::RR ) ) + val_len;
+  // divisor for r1 sort buffer
+  uint64_t div2 = ( uint64_t( sizeof( Node::RR ) ) + val_len )
+    / Knobs::SORT_MERGE_RATIO;
+  // divide by sort + merge buffers
+  memFree /= ( div1 + div2 );
 
   return memFree;
 }
