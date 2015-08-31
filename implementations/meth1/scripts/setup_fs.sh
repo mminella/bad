ecd95355297f6b500296cef326f69d55a86b2b53
David Terei <code@davidterei.com>
2015-08-31 00:43:58 -0700

    Tune mount options for FS.

--- implementations/meth1/scripts/setup_fs.sh
+++ implementations/meth1/scripts/setup_fs.sh
@@ -5,7 +5,7 @@ loc=/mnt/${disk}
 
 sudo mkfs.ext4 -E nodiscard /dev/xvd${disk}
 sudo mkdir -p ${loc}
-sudo mount -o discard /dev/xvd${disk} ${loc}
+sudo mount -o defaults,noatime,discard,nobarrier /dev/xvd${disk} ${loc}
 sudo chown ubuntu:ubuntu ${loc}
 sudo chmod 755 ${loc}
 
