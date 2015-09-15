1bccf5d9faadd8af7d7acd7361296e6fa40ebddb
David Terei <code@davidterei.com>
2015-09-15 16:42:11 -0700

    Remove deprecated memory pool record allocation option.

--- implementations/meth1/libsort/alloc.hh
+++ implementations/meth1/libsort/alloc.hh
@@ -12,34 +12,8 @@
  * - new                     -- 285ms
  * - boost::pool (mutex)     -- 540ms
  */
-#define USE_MPOOL 0
-#define USE_NEW   1
-
 #include "record_common.hh"
 
-#if USE_MPOOL == 1
-#include <iostream>
-#include "MemoryPool.h"
-namespace Rec {
-  using val_type = uint8_t[Rec::VAL_LEN];
-  using Alloc = MemoryPool<val_type>;
-
-  extern Alloc rec_pool;
-
-  inline uint8_t * alloc_val( void )
-  {
-    return (uint8_t *) Rec::rec_pool.allocate();
-  }
-
-  inline void dealloc_val( uint8_t * v )
-  {
-    if ( v != nullptr ) {
-      Rec::rec_pool.deallocate( (val_type *) v );
-    }
-  }
-}
-
-#else /* USE_NEW */
 namespace Rec {
 
   inline uint8_t * alloc_val( void )
@@ -54,6 +28,5 @@ namespace Rec {
     }
   }
 }
-#endif
 
 #endif /* ALLOC_HH */
