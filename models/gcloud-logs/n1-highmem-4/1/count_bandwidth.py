cdd385e01ed62c463350f9d6b48372f0269101ce
ashfall <ashwini.oruganti@gmail.com>
2015-09-21 23:18:50 -0700

    gcloud measurement logs + some helper scripts

---
+++ models/gcloud-logs/n1-highmem-4/1/count_bandwidth.py
@@ -0,0 +1,34 @@
+log_files = ['log_0.txt', 'log_1.txt']
+rows = []
+read_logs = []
+write_logs = []
+unknowns = []
+all_readings = []
+number_of_readings = 0
+for logfile in log_files:
+    f = open(logfile, 'r')
+    for line in f:
+        if line[0] not in 'wr':
+            # print "Not evaluating: ", line
+            pass
+        else:
+            reading = line.split(',')
+            number_of_readings += 1
+            all_readings.append(reading)
+            if reading[0] == 'r':
+                read_logs.append(float(reading[-1]))
+            elif reading[0] == 'w':
+                write_logs.append(float(reading[-1]))
+            else:
+                unknowns.append(reading)
+    f.close()
+
+if unknowns != []:
+    print "Could not recognize these readings: ", unknowns
+else:
+    max_read_bandwidth = max(read_logs)
+    max_write_bandwidth = max(write_logs)
+
+    print "No. of readings: ", number_of_readings
+    print "Max read bandwidth: ", max_read_bandwidth, " MB/s"
+    print "Max write bandwidth: ", max_write_bandwidth, " MB/s"
---
+++ models/gcloud-logs/n1-highmem-8/count_total_bandwidth.py
@@ -0,0 +1,38 @@
+log_files = ['1/log_0.txt', '1/log_1.txt', '1/log_2.txt', '1/log_3.txt',
+             '2/log_0.txt', '2/log_1.txt', '2/log_2.txt', '2/log_3.txt']
+rows = []
+read_logs = []
+write_logs = []
+unknowns = []
+all_readings = []
+number_of_readings = 0
+for logfile in log_files:
+    f = open(logfile, 'r')
+    for line in f:
+        if line[0] not in 'wr':
+            # print "Not evaluating: ", line
+            pass
+        else:
+            reading = line.split(',')
+            number_of_readings += 1
+            all_readings.append(reading)
+            if reading[0] == 'r':
+                read_logs.append(float(reading[-1]))
+            elif reading[0] == 'w':
+                write_logs.append(float(reading[-1]))
+            else:
+                unknowns.append(reading)
+    f.close()
+
+# Getting the max read time
+if unknowns != []:
+    print "Could not recognize these readings: ", unknowns
+else:
+    max_read_bandwidth = max(read_logs)
+    max_write_bandwidth = max(write_logs)
+
+    print "No. of readings: ", number_of_readings
+    print "Max read bandwidth: ", max_read_bandwidth, " MB/s"
+    print "Max write bandwidth: ", max_write_bandwidth, " MB/s"
+
+
