a1c441fc3345de9e9bcf31f0aa9bd2a08b314d97
ashfall <ashwini.oruganti@gmail.com>
2015-09-22 00:18:39 -0700

    Cleaner file handling

--- models/gcloud-logs/n1-highmem-32/1/count_bandwidth.py
+++ models/gcloud-logs/n1-highmem-32/1/count_bandwidth.py
@@ -6,24 +6,23 @@ unknowns = []
 all_readings = []
 number_of_readings = 0
 for logfile in log_files:
-    f = open(logfile, 'r')
-    for line in f:
-        if line[0] not in 'wr':
-            # print "Not evaluating: ", line
-            pass
-        else:
-            reading = line.split(',')
-            number_of_readings += 1
-            all_readings.append(reading)
-            if reading[0] == 'r':
-                read_logs.append(float(reading[-1]))
-            elif reading[0] == 'w':
-                write_logs.append(float(reading[-1]))
+    with open(logfile, 'r') as f:
+        for line in f:
+            if line[0] not in 'wr':
+                # print "Not evaluating: ", line
+                pass
             else:
-                unknowns.append(reading)
-    f.close()
+                reading = line.split(',')
+                number_of_readings += 1
+                all_readings.append(reading)
+                if reading[0] == 'r':
+                    read_logs.append(float(reading[-1]))
+                elif reading[0] == 'w':
+                    write_logs.append(float(reading[-1]))
+                else:
+                    unknowns.append(reading)
 
-# Getting the max read time
+
 if unknowns != []:
     print "Could not recognize these readings: ", unknowns
 else:
