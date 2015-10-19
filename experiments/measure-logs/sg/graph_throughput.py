2050e2d3f0e2893aade0b41493f75a0225dbdeed
David Terei <code@davidterei.com>
2015-10-18 17:27:23 -0700

    Improvements to longitudinal measurement graphing scripts.

--- experiments/measure-logs/sg/graph_throughput.py
+++ experiments/measure-logs/sg/graph_throughput.py
@@ -1,95 +1,133 @@
+#!/usr/bin/env python2
+#
 # Usage: `python graph_throughput.py path/to/experiments/measure_logs/sg/
-# (with the trailing '/' in the end)
 
+import collections as coll
 import datetime
-import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
-import numpy
+import matplotlib.pyplot as plt
+import numpy as np
 import os
+import pylab
 import sys
 
-base_sg_dir = sys.argv[1]
-timestamp_file = '{}README.md'.format(base_sg_dir)
-log_files = []
+# constants
 YEAR = 2015
+TIMESTAMP_FILE = '{}README.md'
+FILE_OUT = 'graph.pdf'
 
+# check usage
+if len(sys.argv) != 2:
+  print "Usage:", sys.argv[0], "<path to experiment results>"
+  exit(1)
+base_dir = sys.argv[1]
+if not base_dir.endswith('/'):
+  base_dir += '/'
 
-for root, dirs, files in os.walk(base_sg_dir):
-    if root != base_sg_dir and \
-            root[:len(base_sg_dir+'14')] != '{}14'.format(base_sg_dir):
-        # We don't have a timestamp for 14.
+def load_logfiles(root_dir):
+    """
+    root_dir: the directory we are loading all data from
+    """
+    logfiles = []
+    for root, dirs, files in os.walk(root_dir):
+        if root == root_dir:
+            continue
         for filename in files:
             if filename != 'log_cpu.txt' and filename[-3:] != 'pdf':
-                log_files.append('{}/{}'.format(root, filename))
+                logfiles.append('{}/{}'.format(root, filename))
+    return logfiles
 
+def load_timestamps(root_dir):
+    """
+    root_dir: the directory we are loading all data from
+    """
+    timestamps = {}
+    ts_file = TIMESTAMP_FILE.format(root_dir)
+    with open(ts_file, 'r') as f:
+        lines = f.readlines()[2:]
+        for line in lines:
+            # This is a line containing the timestamp we want.
+            # It is of the form "* 26 -- 8/31 -- 3am"
+            exp, d, t = line[2:].strip().split(' -- ')
+            t = t.strip()
+            month, day = d.split('/')
+            if t[-2:] == 'am':
+                hour = int(t[0:len(t)-2]) % 12
+            elif t[-2:] == 'pm':
+                hour = int(t[0:len(t)-2]) % 12 + 12
+            timestamps[exp] = datetime.datetime(
+                YEAR, int(month), int(day), hour
+            )
+    return timestamps
+
+def print_summary(data_set):
+    results = sorted([z[1] for z in data_set])[0:]
+    timestamps = set([z[0] for z in data_set])
+    print "Samples:", len(data_set)
+    print "Runs:", len(timestamps)
+    print "Mean:", np.mean(results)
+    print "Median:", np.median(results)
+    print "Std:", np.std(results)
+    print "Min:", np.min(results)
+    print "Max:", np.max(results)
+
+def datetime_to_date(dt):
+    return datetime.date(dt.year, dt.month, dt.day)
 
 def make_graph(data_set, exp_name):
     """
     data_set: a dict of experiment setup string vs. list of experiment
     values.
     """
-    labels_list = []
-    x = []
-    y = []
-    timestamp_result_dict = {}
-
+    # group by timestamp
+    data_by_timestamp = coll.defaultdict(list)
     for timestamp, result in data_set:
-        # We now group the data as per the timestamp for every experiment run.
-        if timestamp_result_dict.get(timestamp):
-            timestamp_result_dict[timestamp].append(result)
-        else:
-            timestamp_result_dict[timestamp] = [result]
+        ts = datetime_to_date(timestamp)
+        data_by_timestamp[ts].append(result)
 
-    for ts, results_list in sorted(timestamp_result_dict.iteritems()):
-        # Make sure the data we are plotting is sorted by timestamp.
-        x.append(ts)
-        y.append(results_list)
+    # produce x, y values for graph
+    xv = []
+    yv = []
+    start = sorted(data_by_timestamp.iteritems())[0][0]
+    end = sorted(data_by_timestamp.iteritems())[-1][0]
+    days = (end - start).days + 1
+    dlist = [start + datetime.timedelta(days=x) for x in range(0, days)]
+    for d in dlist:
+      xv.append(d)
+      if d in data_by_timestamp:
+        yv.append(data_by_timestamp[d])
+      else:
+        # add in missing timestamps
+        yv.append([])
 
+    # plot graph
     fig, ax = plt.subplots(1)
-    labels_list.append(exp_name)
+    plt.boxplot(yv)
     fig.autofmt_xdate()
-    plt.boxplot(y)
     ax.xaxis_date()
-    plt.xticks(range(1, len(x)+1), x, rotation=17)
 
-    # Labels and legend
-    plt.ylabel('Throughput (in MB/s)')
-    plt.xlabel('Time')
-    plt.legend(labels_list)
-    plt.show()
+    # x-axis ticks
+    xsubset = range(0, len(xv), 10)
+    xlabels = [xv[i] for i in xsubset]
+    plt.xticks(xsubset, xlabels, rotation=40)
+
+    # y-axis range
+    pylab.ylim([000,500])
+
+    # labels and legend
+    plt.ylabel('Throughput (MB/s)')
+    plt.xlabel('Date of Measurment')
+    plt.title('Sequential Disk IO Measurements')
+    # plt.legend([exp_name])
+
+    # Produce output
     # XXX To save a plot, instead of viewing it, replace `plt.show()` with:
-    # `plt.savefig('path/to/save/figure.pdf`)
+    # plt.show()
+    plt.savefig(FILE_OUT)
 
-
-def get_timestamp_dict(timestamps):
-    with open(timestamp_file, 'r') as f:
-        for line in f:
-            if line.strip() == "* 27, Mon Aug 31 11:45:24 PDT 2015, ap-southeast-1a, i2.xlarge":
-                # hardcode this anomaly
-                timestamps['27'] = datetime.datetime(2015, 8, 31, 11, 45, 24)
-
-            elif line[0] == '*':
-                # This is a line containing the timestamp we want.
-                # It is of the form "* 26 -- 8/31 -- 3am"
-                ts = line[2:]
-                exp, d, t = ts.strip().split(' -- ')
-                t = t.strip()  # some extra whitespace
-                month, day = d.split('/')
-                if t[-2:] == 'am':
-                    hour = int(t[0])
-                elif t[-2:] == 'pm':
-                    hour = int(t[0]) + 12
-                # TODO provide timezone.
-                timestamps[exp] = datetime.datetime(
-                    YEAR, int(month), int(day), hour
-                )
-
-
-def pick_an_experiment_to_plot(data_set):
-    exp_to_plot = ''
-    print "Please choose an experiment from the following:"
-    for exp_name in data_set.keys():
-        print exp_name
+def pick_an_experiment_to_plot(experiments):
+    for name in experiments.keys():
+        print name
     exp_to_plot = raw_input("Please choose an experiment from the above : ")
     return exp_to_plot
 
@@ -98,25 +136,25 @@ if __name__ == '__main__':
     exp_result_list: A dict mapping from the exp setup
     string to a list of the resulting throughputs.
     """
-    exp_result_dict = {}
-    timestamps = {}
-    get_timestamp_dict(timestamps)
-    for logfile in log_files:
+    # load all needed data
+    logfiles = load_logfiles(base_dir)
+    timestamps = load_timestamps(base_dir)
+    experiments = coll.defaultdict(list)
+
+    # process data, grouping correctly by experiment
+    for logfile in logfiles:
         exp_no = logfile.rsplit('/', 3)[1]
         timestamp = timestamps[exp_no]
         with open(logfile, 'r') as f:
             for line in f:
                 if line[0] not in 'wr':
-                    # Assuming the non-setup/log lines are not going to start
-                    # with a 'w' or 'r'
-                    pass
-                else:
-                    exp_log = line.rsplit(',', 1)
-                    result = [timestamp, float(exp_log[1])]
-                    if exp_result_dict.get(exp_log[0]):
-                        exp_result_dict[exp_log[0]].append(result)
-                    else:
-                        exp_result_dict[exp_log[0]] = [result]
+                    # Skip over non-experiment lines
+                    continue
+                experiment = line.rsplit(',', 1)
+                result = [timestamp, float(experiment[1])]
+                experiments[experiment[0]].append(result)
 
-    exp_to_plot = pick_an_experiment_to_plot(exp_result_dict)
-    make_graph(exp_result_dict[exp_to_plot], exp_to_plot)
+    # plot an experiment
+    exp_to_plot = pick_an_experiment_to_plot(experiments)
+    print_summary(experiments[exp_to_plot])
+    make_graph(experiments[exp_to_plot], exp_to_plot)
