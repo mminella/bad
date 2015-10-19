e97ef0e5a955d2694354a6d74865f08f71409a61
David Terei <code@davidterei.com>
2015-10-18 18:24:26 -0700

    Remove O_DIRECT option from meth1_node.

--- implementations/meth1/app/meth1_node.cc
+++ implementations/meth1/app/meth1_node.cc
@@ -18,18 +18,18 @@
 using namespace std;
 using namespace meth1;
 
-void run( vector<string> files, string port, string odirect )
+void run( vector<string> files, string port )
 {
-  Node node{files, port, to_bool( odirect )};
+  Node node{files, port};
   node.Initialize();
   node.Run();
 }
 
 void check_usage( const int argc, const char * const argv[] )
 {
-  if ( argc < 4 ) {
+  if ( argc < 3 ) {
     throw runtime_error( "Usage: " + string( argv[0] ) +
-                         " [port] [odirect] [file...]" );
+                         " [port] [file...]" );
   }
 }
 
@@ -37,7 +37,7 @@ int main( int argc, char * argv[] )
 {
   try {
     check_usage( argc, argv );
-    run( {argv+3, argv+argc}, argv[1], argv[2] );
+    run( {argv+2, argv+argc}, argv[1] );
   } catch ( const exception & e ) {
     print_exception( e );
     return EXIT_FAILURE;
